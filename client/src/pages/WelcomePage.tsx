b1ed73e07038a9acc61ecc6498c8f896ef45e948
finbarr12 <ogbonnafinbarr@gmail.com>
2024-02-08 15:41:26 +0100

    added

--- client/src/pages/WelcomePage.tsx
+++ client/src/pages/WelcomePage.tsx
@@ -4,7 +4,15 @@ const WelcomePage = () => {
   return (
     <>
       <Container>
-        <Wrapper></Wrapper>
+        <Wrapper>
+          <Left>
+            <h1>Use of English (GST 104)</h1>
+            <p>Online CBT Examination</p>
+          </Left>
+          <Right>
+            <h3>Enter Your Credentials</h3>
+          </Right>
+        </Wrapper>
       </Container>
     </>
   );
@@ -12,10 +20,31 @@ const WelcomePage = () => {
 
 export default WelcomePage;
 
+const Left = styled.div`
+  color: white;
+  margin-top: 250px;
+  margin-left: 89px;
+
+  h1 {
+    margin: 0;
+    font-size: 50px;
+  }
+
+  p {
+    font-size: 15px;
+  }
+`;
+const Right = styled.div`
+  color: white;
+  margin-right: 89px;
+`;
+
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
   background-color: rgba(31, 28, 59, 0.6);
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Container = styled.div`
