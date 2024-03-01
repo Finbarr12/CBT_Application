17570d150f10cf79effeee66893a56acc5a9b10e
finbarr12 <ogbonnafinbarr@gmail.com>
2024-03-01 15:09:05 +0100

    new

--- Server/config/app.ts
+++ Server/config/app.ts
@@ -1,7 +1,9 @@
 import express, { Application } from "express";
 import router from "../Router/router";
+import cors from "cors";
 
 export const ServerApp = (app: Application) => {
+  app.use(cors());
   app.use(express.json());
   app.use("/api/v1", router);
 };
